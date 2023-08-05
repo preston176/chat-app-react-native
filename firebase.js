de384a6d219363c2254b3fa07b35b4ed66435c37
preston176 <prestonnyamweya@gmail.com>
2023-08-06 02:58:45 +0300

    firebase and react nav installed

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBrSE2HQ-Nz7trpj-0vVhDfVqwRoAM-qGI",
+    authDomain: "tectalk-e05f6.firebaseapp.com",
+    projectId: "tectalk-e05f6",
+    storageBucket: "tectalk-e05f6.appspot.com",
+    messagingSenderId: "614327329210",
+    appId: "1:614327329210:web:a0c93f5cb2427734903d83"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
