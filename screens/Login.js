df2489277b4fcb7a1307a42d99565646a5882102
preston176 <prestonnyamweya@gmail.com>
2023-08-06 13:46:56 +0300

    logo in login

--- screens/Login.js
+++ screens/Login.js
@@ -3,13 +3,18 @@ import React from 'react'
 import { Button, Input, Image } from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
 
+
 const Login = () => {
-  return (
-    <View>
-    <StatusBar style="light"/>
-      <Text>Login</Text>
-    </View>
-  )
+    return (
+        <View>
+            <StatusBar style="light" />
+            <Image source={{
+                uri: "https://png.pngtree.com/template/20190927/ourmid/pngtree-initial-p-chat-logo-design-vector-illustration-image_310613.jpg",
+            }}
+                style={{ width: 200, height: 200 }}
+            />
+        </View>
+    )
 }
 
 export default Login
