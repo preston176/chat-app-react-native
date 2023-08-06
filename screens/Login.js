2954d1cac7fe4fc71c2a47a3f53e0dd5144db3c0
preston176 <prestonnyamweya@gmail.com>
2023-08-06 14:04:47 +0300

    reg button

--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Input, Image } from '@rneui/base';
 import { StatusBar } from 'expo-status-bar';
 
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = () => { }
+
+
     return (
-        <View>
+        <View style={styles.container}>
             <StatusBar style="light" />
             <Image source={{
                 uri: "https://png.pngtree.com/template/20190927/ourmid/pngtree-initial-p-chat-logo-design-vector-illustration-image_310613.jpg",
@@ -14,8 +20,11 @@ const Login = () => {
                 style={{ width: 200, height: 200 }}
             />
             <View style={styles.inputContainer}>
-                <Input placeholder='Enter your email' />
+                <Input placeholder='Enter your email' autoFocus type="Email" value={email} onChangeText={text => setEmail(text)} />
+                <Input placeholder='Enter your Password' secureTextEntry type="Password" onChangeText={pass => setPassword(pass)} value={password} />
             </View>
+            <Button title="Login" onPress={signIn} containerStyle={styles.button} />
+            <Button title="Register" type='outline' containerStyle={styles.button} />
         </View>
     )
 }
@@ -23,7 +32,13 @@ const Login = () => {
 export default Login
 
 const styles = StyleSheet.create({
+    container: {
+        
+    },
     inputContainer: {
+
+    },
+    button: {
 
     }
 })
