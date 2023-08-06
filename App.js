6f6a7f34cb77cfbb0bde318060be4fdc6cd1ec97
preston176 <prestonnyamweya@gmail.com>
2023-08-07 00:59:38 +0300

    addchat

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './screens/Login';
 import Register from './screens/Register';
 import Home from './screens/Home';
+import AddChat from './components/AddChat';
+
 
 
 const Stack = createNativeStackNavigator()
@@ -21,11 +23,12 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-         initialRouteName='Home'
+        initialRouteName='Home'
         screenOptions={globalScreenOptions}>
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='Register' component={Register} />
         <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='AddChat' component={AddChat} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/Home.js
+++ screens/Home.js
@@ -42,7 +42,7 @@ const Home = ({ navigation }) => {
                 <TouchableOpacity activeOpacity={0.5}>
                     <AntDesign name='camerao' size={24} color="black" />
                 </TouchableOpacity>
-                <TouchableOpacity activeOpacity={0.5}>
+                <TouchableOpacity onPress={() => navigation.navigate("AddChat")} activeOpacity={0.5}>
                 <SimpleLineIcons name='pencil' size={24} color="black" />
 
                 </TouchableOpacity>
