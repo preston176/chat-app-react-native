b68e2e35b908e2584033704befe1cee09c0dd9ca
preston176 <prestonnyamweya@gmail.com>
2023-08-07 02:06:38 +0300

    placeholder URI

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,11 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useLayoutEffect } from 'react'
+import { Avatar } from "react-native-elements"
 
 const Chat = ({ navigation, route }) => {
     useLayoutEffect(() => {
         navigation.setOptions({
             title: "Chat",
-            headerTitleAlign: "left"
+            headerTitleAlign: "left",
+            headerTitle: () => (
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center"
+                    }}
+                >
+                    <Avatar rounded source={{
+                        uri: "...."
+                    }} />
+                    <Text>{route.params.chatName}</Text>
+                </View>
+            )
         });
     }, [navigation])
 
