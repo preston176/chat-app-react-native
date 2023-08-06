3386811f671e53ba9e86ba5c5b0143b414e4804c
preston176 <prestonnyamweya@gmail.com>
2023-08-06 03:03:45 +0300

    login stylesheet

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
-    </div>
+    <View>
+      <Text>Login</Text>
+    </View>
   )
 }
 
 export default Login
+
+const styles = StyleSheet.create({})
