5787c83428482cf77338db4057c5129dd7be0cdc
preston176 <prestonnyamweya@gmail.com>
2023-08-06 21:11:43 +0300

    props

--- components/CustomListitem.js
+++ components/CustomListitem.js
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { ListItem, Avatar } from 'react-native-elements'
 
-const CustomListitem = () => {
+const CustomListitem = ({id, chatName, enterChat}) => {
     return (
         <ListItem>
             <Avatar
