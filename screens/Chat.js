d174fbc775d5e56da1af4b38c98a97ff2a234515
preston176 <prestonnyamweya@gmail.com>
2023-08-07 11:52:13 +0300

    use layout effect

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,35 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { Avatar } from "react-native-elements"
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Avatar, TouchableOpacity } from "react-native-elements";
+import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
 
 const Chat = ({ navigation, route }) => {
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            title: "Chat",
-            headerTitleAlign: "left",
-            headerTitle: () => (
-                <View
-                    style={{
-                        flexDirection: "row",
-                        alignItems: "center"
-                    }}
-                >
-                    <Avatar rounded source={{
-                        uri: "...."
-                    }} />
-                    <Text>{route.params.chatName}</Text>
-                </View>
-            )
-        });
-    }, [navigation])
 
-    return (
-        <View>
-            <Text>{route.params.chatName}</Text>
-        </View>
-    )
-}
+//   useEffect(() => {
+//     navigation.setOptions({
+//       title: "Chat",
+//       headerTitleAlign: "left",
+//       headerTitle: () => (
+//         <View
+//           style={{
+//             flexDirection: "row",
+//             alignItems: "center"
+//           }}
+//         >
+//           <Avatar rounded
+//           //  source={{ uri: "...." }} 
+//           />
+//           <Text style={{
+//             color: "white",
+//             marginLeft: 10,
+//             fontWeight: "700"
+//           }}>{route.params.chatName}</Text>
+//         </View>
+//       ),
+//       headerLeft: () => (
+//         <TouchableOpacity style={{
+//           marginLeft: 10
+//         }}
+//         onPress={navigation.goBack}
+//         >
+//           <AntDesign name='arrowleft'
+//             size={24} color="white" />
+//         </TouchableOpacity>
+//       ),
+//       headerRight: () => (
+//         <View
+//           style={{
+//             flexDirection: "row",
+//             justifyContent: "space-between",
+//             width: 80,
+//             marginRight: 20,
+//           }}
+//         >
+//           <TouchableOpacity>
+//             <FontAwesome name='video-camera' size={24} color="white" />
+//           </TouchableOpacity>
+//           <TouchableOpacity>
+//             <Ionicons name='call' size={24} color="white" />
+//           </TouchableOpacity>
+//         </View>
+//       )
+//     });
+//   }, [navigation, route.params.chatName]);
 
-export default Chat
+  return (
+    <View>
+      <Text>{route.params.chatName}</Text>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Chat;
+
+const styles = StyleSheet.create({});
