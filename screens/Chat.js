d7c3d6f3473ab094c286cf998143147ebd2ccdfd
preston176 <prestonnyamweya@gmail.com>
2023-08-07 13:32:00 +0300

    touchable opacity

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,68 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect } from 'react';
-import { Avatar, TouchableOpacity } from "react-native-elements";
+import { StyleSheet, Text, View, SafeAreaView, KeyboardAvoidingView, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Avatar } from "react-native-elements";
 import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons";
 
 const Chat = ({ navigation, route }) => {
 
-//   useEffect(() => {
-//     navigation.setOptions({
-//       title: "Chat",
-//       headerTitleAlign: "left",
-//       headerTitle: () => (
-//         <View
-//           style={{
-//             flexDirection: "row",
-//             alignItems: "center"
-//           }}
-//         >
-//           <Avatar rounded
-//           //  source={{ uri: "...." }} 
-//           />
-//           <Text style={{
-//             color: "white",
-//             marginLeft: 10,
-//             fontWeight: "700"
-//           }}>{route.params.chatName}</Text>
-//         </View>
-//       ),
-//       headerLeft: () => (
-//         <TouchableOpacity style={{
-//           marginLeft: 10
-//         }}
-//         onPress={navigation.goBack}
-//         >
-//           <AntDesign name='arrowleft'
-//             size={24} color="white" />
-//         </TouchableOpacity>
-//       ),
-//       headerRight: () => (
-//         <View
-//           style={{
-//             flexDirection: "row",
-//             justifyContent: "space-between",
-//             width: 80,
-//             marginRight: 20,
-//           }}
-//         >
-//           <TouchableOpacity>
-//             <FontAwesome name='video-camera' size={24} color="white" />
-//           </TouchableOpacity>
-//           <TouchableOpacity>
-//             <Ionicons name='call' size={24} color="white" />
-//           </TouchableOpacity>
-//         </View>
-//       )
-//     });
-//   }, [navigation, route.params.chatName]);
+  const [input, setInput] = useState('')
+
+  const sendMessage = () => {
+
+  }
 
   return (
-    <View>
-      <Text>{route.params.chatName}</Text>
-    </View>
+    <SafeAreaView>
+      <KeyboardAvoidingView
+        padding
+        style={styles.container}
+        keyboardVerticalOffset={90}
+      >
+        <>
+          <ScrollView>
+            {/* chat */}
+          </ScrollView>
+          <View style={styles.footer}>
+            <TextInput
+              placeholder='Message'
+              style={styles.textInput}
+              value={input}
+              onChangeText={text => setInput(text)}
+            />
+            <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+            <Ionicons name='send' size={24} color="#2B68E6"/>
+</TouchableOpacity>
+          </View>
+        </>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
 export default Chat;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  footer: {},
+  textInput: {},
+});
